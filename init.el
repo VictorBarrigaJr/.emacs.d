e0a1949d4edfbde70a69677f9f70a7a59fceb3ee
Victor Barriga <victorbarriga@live.com>
2014-10-06 02:08:49 -0700

    added features to file saving and rules for autosaving file and versions

--- init.el
+++ init.el
@@ -192,7 +192,25 @@
 ;; Ctrl X-Ctrl S button dangerously close to Ctrl X-Ctrl C
 (setq confirm-kill-emacs 'yes-or-no-p)
 
+;; group: Files                      ;;
+(setq large-file-warning-threshold 100000000) ;; size in bytes
 
+;; GROUP: Files -> Back up            ;;
+(defvar backup-directory "~/.backups")
+(if (not (file-exists-p backup-directory))
+    (make-directory backup-directory t))
+(setq 
+ make-backup-files t        ; backup a file the first time it is saved
+ backup-directory-alist `((".*" . ,backup-directory)) ; save in ~/.backups
+ backup-by-copying t     ; copy the current file into backup directory
+ version-control t   ; version numbers for backup files
+ delete-old-versions t   ; delete unnecessary versions
+ kept-old-versions 6     ; oldest versions to keep (default: 2)
+ kept-new-versions 9 ; newest versions to keep (default: 2)
+ auto-save-default t ; auto-save every buffer that visits a file
+ auto-save-timeout 20 ; idle time before auto-save (default: 30)
+ auto-save-interval 200 ; number of keystrokes between auto-saves (default: 300)
+ )
 ;; c cc-mode
 (add-hook 'c-mode-common-hook
           (lambda ()
