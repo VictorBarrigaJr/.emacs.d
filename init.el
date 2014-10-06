ee36caaeea49b2813b15adb98ba5bb24faaaa330
Victor Barriga <victorbarriga@live.com>
2014-10-06 11:25:49 -0700

    edited smartparens package to be global

--- init.el
+++ init.el
@@ -102,10 +102,7 @@
 
 ;; Package: smartparens
 (require 'smartparens-config)
-(setq sp-base-key-bindings 'paredit)
-(setq sp-autoskip-closing-pair 'always)
-(setq sp-hybrid-kill-entire-symbol nil)
-(sp-use-paredit-bindings)
+(show-smartparens-global-mode +1)
 
 (require 'smooth-scrolling)
 (require 'uniquify)
