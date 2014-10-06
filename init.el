11a941c9fcdcad682fb74f68a25fba266d39b7d2
Victor Barriga <victorbarriga@live.com>
2014-10-06 12:28:23 -0700

    fixed golden-ratio settings

--- init.el
+++ init.el
@@ -227,36 +227,7 @@
 (global-set-key [f11] 'fullscreen)
 ;;golden ratio package
 (require 'golden-ratio)
-(add-to-list 'golden-ratio-exclude-modes "ediff-mode")
-(add-to-list 'golden-ratio-exclude-modes "helm-mode")
-(add-to-list 'golden-ratio-exclude-modes "dired-mode")
-(add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
-
-(defun pl/helm-alive-p ()
-  (if (boundp 'helm-alive-p)
-      (symbol-value 'helm-alive-p)))
-
-;; do not enable golden-raio in thses modes
-(setq golden-ratio-exclude-modes '("ediff-mode"
-                                   "gud-mode"
-                                   "gdb-locals-mode"
-                                   "gdb-registers-mode"
-                                   "gdb-breakpoints-mode"
-                                   "gdb-threads-mode"
-                                   "gdb-frames-mode"
-                                   "gdb-inferior-io-mode"
-                                   "gud-mode"
-                                   "gdb-inferior-io-mode"
-                                   "gdb-disassembly-mode"
-                                   "gdb-memory-mode"
-                                   "magit-log-mode"
-                                   "magit-reflog-mode"
-                                   "magit-status-mode"
-                                   "IELM"
-                                   "eshell-mode" "dired-mode"))
-
 (golden-ratio-mode)
-
 
 ;; notifies you once emacs is ready to use and the load time 
 (defun dim:notify-startup-done ()
