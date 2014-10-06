b8bd6d7a599378a1ef12dc9f3f0ac04fe5c6965a
Victor Barriga <victorbarriga@live.com>
2014-10-06 02:52:22 -0700

    removed nyan cat due to error

--- init.el
+++ init.el
@@ -70,7 +70,7 @@
          company-mode el-get emacs-goodies-el escreen fill-column-indicator 
          fuzzy flymake gnus ggtags iedit minimap notify package popup 
          smartparens smex switch-window volatile-highlights undo-tree 
-         yasnippet nyan-mode)))
+         yasnippet)))
 
 (el-get 'sync my:el-get-packages)
 
@@ -325,12 +325,6 @@
 
 ;; GROUP: Programming -> Tools -> Flycheck ;;
 (require 'flymake)
-
-;; nyan-mode Environment -> Frames -> Nyan ;;
-;; only turn on if a window system is available
-;; this prevents error under terminal that does not support X
-(case window-system
-  ((x w32) (nyan-mode)))
 
 ;; full screen
 (defun fullscreen ()
